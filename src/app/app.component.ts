830f07dd2eebbaf581525e02510eced6d7690eaf
Carlos Leonardo <carlos.leonardo.goes@gmail.com>
2024-09-26 15:33:07 -0300

    Inclusão inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'demo-upload';
+  arquivos: string[] = [];
+
+  onFileChange() {
+    console.log(this.arquivos);
+  }
+
+  aoEnviar(e: Event) {
+    e.preventDefault();
+  }
+  title = 'Demonstração de Upload';
 }
